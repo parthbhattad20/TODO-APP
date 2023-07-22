9166c58c30f060c581b377b7c570f69bf3644826
Parth Bhattad <bhattadparth20@gmail.com>
2023-07-22 13:00:36 +0530

    git uri

--- backend/server.js
+++ backend/server.js
@@ -1,80 +1,84 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect('mongodb+srv://Parth:Parth@20@cluster0.nlyt64i.mongodb.net/?retryWrites=true&w=majority', {
-	useNewUrlParser: true, 
-	useUnifiedTopology: true 
-}).then(() => console.log("Connected to MongoDB")).catch(console.error);
+mongoose
+    .connect(
+        "mongodb+srv://Parth2:Parth20@cluster0.nlyt64i.mongodb.net/?retryWrites=true&w=majority",
+        {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        }
+    )
+    .then(() => console.log("Connected to MongoDB"))
+    .catch(console.error);
 
+const Todo = require("./models/Todo");
 
-const Todo = require('./models/Todo');
+app.get("/todos", async (req, res) => {
+    const todos = await Todo.find();
 
-app.get('/todos', async (req, res) => {
-	const todos = await Todo.find();
-
-	res.json(todos);
+    res.json(todos);
 });
 
-app.post('/todo/new', (req, res) => {
-	const todo = new Todo({
-		text: req.body.text
-	})
+app.post("/todo/new", (req, res) => {
+    const todo = new Todo({
+        text: req.body.text,
+    });
 
-	todo.save();
+    todo.save();
 
-	res.json(todo);
+    res.json(todo);
 });
 
-app.delete('/todo/delete/:id', async (req, res) => {
-	const result = await Todo.findByIdAndDelete(req.params.id);
+app.delete("/todo/delete/:id", async (req, res) => {
+    const result = await Todo.findByIdAndDelete(req.params.id);
 
-	res.json({result});
+    res.json({ result });
 });
 
-app.get('/todo/complete/:id', async (req, res) => {
-	try {
-	  const todo = await Todo.findById(req.params.id);
-  
-	  if (!todo) {
-		return res.status(404).json({ message: 'Todo not found' });
-	  }
-  
-	  todo.complete = !todo.complete;
-  
-	  await todo.save();
-  
-	  res.json(todo);
-	} catch (error) {
-	  res.status(500).json({ message: 'Internal server error' });
-	}
-  });
-  
-  
+app.get("/todo/complete/:id", async (req, res) => {
+    try {
+        const todo = await Todo.findById(req.params.id);
 
-app.put('/todo/update/:id', async (req, res) => {
-	try {
-	  const todo = await Todo.findById(req.params.id);
-  
-	  if (!todo) {
-		return res.status(404).json({ error: 'Todo not found' });
-	  }
-  
-	  todo.text = req.body.text;
-  
-	  await todo.save();
-  
-	  res.json(todo);
-	} catch (error) {
-	  console.error(error);
-	  res.status(500).json({ error: 'An error occurred while updating the todo' });
-	}
-  });
-  
+        if (!todo) {
+            return res.status(404).json({ message: "Todo not found" });
+        }
+
+        todo.complete = !todo.complete;
+
+        await todo.save();
+
+        res.json(todo);
+    } catch (error) {
+        res.status(500).json({ message: "Internal server error" });
+    }
+});
+
+app.put("/todo/update/:id", async (req, res) => {
+    try {
+        const todo = await Todo.findById(req.params.id);
+
+        if (!todo) {
+            return res.status(404).json({ error: "Todo not found" });
+        }
+
+        todo.text = req.body.text;
+
+        await todo.save();
+
+        res.json(todo);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({
+            error: "An error occurred while updating the todo",
+        });
+    }
+});
 
 app.listen(3001);
